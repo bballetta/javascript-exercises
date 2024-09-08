11d45bda12632647d67353693a6836e4a356fcc2
bballetta <bretb310@gmail.com>
2024-09-08 13:21:01 -0500

    JS exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let splitString = str.split("");
+  console.log(splitString);
+  let reverseArray = splitString.reverse();
+  console.log(reverseArray);
+  let joinArray = reverseArray.join("");
+  console.log(joinArray);
+  return joinArray;
 };
 
+reverseString("123! abc! Hello, Odinite.");
 // Do not edit below this line
 module.exports = reverseString;
