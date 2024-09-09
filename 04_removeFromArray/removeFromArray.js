952585922f6ed64204ba50951cfe563b4aa29d2b
Bret Balletta <bballetta@tesla.com>
2024-09-09 10:44:12 -0500

    JS exercise 04 - second test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,14 @@
-const removeFromArray = function(array, elements) {
-    const index = array.indexOf(elements);
-    if (index > -1); {
-      array.splice(index, 1);  
-    }
+const removeFromArray = function(array, ...elements) {
+    let index = array.indexOf(...elements);
+    for (const i of elements) {
+      if (index > -1); {
+        array.splice(index,1);
+        index--;  
+      };
+    };  
     return (array);
-    console.log(array);
 };
 
-//removeFromArray([1,2,3,4],3);
+removeFromArray([1,2,3,4],3,2);
 // Do not edit below this line
 module.exports = removeFromArray;
