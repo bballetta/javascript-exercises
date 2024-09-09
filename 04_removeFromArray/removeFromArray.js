5724aed6787124add71e005cf1596880cadce9f3
Bret Balletta <bballetta@tesla.com>
2024-09-09 10:16:38 -0500

    JS exercise 04 - remove one element from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elements) {
+    const index = array.indexOf(elements);
+    if (index > -1); {
+      array.splice(index, 1);  
+    }
+    return (array);
+    console.log(array);
 };
 
+//removeFromArray([1,2,3,4],3);
 // Do not edit below this line
 module.exports = removeFromArray;
