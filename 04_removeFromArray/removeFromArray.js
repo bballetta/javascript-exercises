5548ebd3dffd50b641836fa3dec8c46a83e68dfa
Bret Balletta <bballetta@tesla.com>
2024-09-10 10:53:09 -0500

    Add simplest solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-const removeFromArray = function(array, ...elements) {
+/*const removeFromArray = function(array, ...elements) {
     array = array.filter((item, index) => array.indexOf(item) === index);
     for (const element of elements) {
       let index = array.indexOf(element);
@@ -8,7 +8,11 @@ const removeFromArray = function(array, ...elements) {
         };
     };
     return (array);
-};    
+};*/    
+
+const removeFromArray = function(array, ...elements) {
+return array.filter(val => !elements.includes(val))
+}
 
 removeFromArray([1,2,3,4],7,2);
 // Do not edit below this line
