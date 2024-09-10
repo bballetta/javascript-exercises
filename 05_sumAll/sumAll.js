f5b96547e47b0f484c70c5d6b1d55c392adc5676
Bret Balletta <bballetta@tesla.com>
2024-09-10 11:51:13 -0500

    JS exercise 05 - test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(lowInt, highInt) {
+    let arr = [];
+    for (i = lowInt; i <= highInt; i++) {
+        arr.push(i);
+    };
+    const sum = arr.reduce((partialSum, a) => partialSum + a, 0);
+    console.log(arr);
+    console.log(sum);
+    return(sum);
+    
 };
 
+sumAll(1,4)
 // Do not edit below this line
 module.exports = sumAll;
