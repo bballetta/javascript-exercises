a9e5e7f7264feb5d1d6810d2eebbc3369dea66cc
Bret Balletta <bballetta@tesla.com>
2024-09-11 12:44:53 -0500

    JS exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,38 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const tempVal = (temp - 32) * (5 / 9);
+  if (tempVal - Math.floor(tempVal) != 0) {
+    const rounded = tempVal.toFixed(1); 
+  return parseFloat(rounded);
+  }
+  else {
+    return tempVal;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const tempVal = (temp * 9 / 5 + 32);
+  if (tempVal - Math.floor(tempVal) != 0) {
+    const rounded = tempVal.toFixed(1);
+  return parseFloat(rounded);
+  }
+  else {
+    return tempVal;
+  }
 };
 
+//convertToCelsius(100);
+convertToFahrenheit(0);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+/* TOP solution 
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+};
+
+const convertToFahrenheit = function (celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+}; */
